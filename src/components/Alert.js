b4f832cc002cbaca02842e13bc6f4b01be0f5504
Alex <surfer.ru@mail.ru>
2019-09-26 19:19:14 +0400

    add files project

---
+++ src/components/Alert.js
@@ -0,0 +1,20 @@
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const Alert = () => {
+    const {alert, hide} = useContext(AlertContext)
+
+    if(!alert.visible){
+        return null
+    }
+
+    return(
+        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible fade show`}>
+            <strong>Внимание! </strong> 
+            {alert.text}
+            <button onClick={hide} type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">Лучшее React приложение</h1>
+            <p className="lead">Версия приложения 1.0.0</p>
+            <hr className="my-4" />
+            <p>Приятного пользования</p>
+        </div>
+    )
+}
